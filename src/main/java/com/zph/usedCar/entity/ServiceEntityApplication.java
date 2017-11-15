49ab90ce49aaa666d76354fb8b741e2b472d29d7
zhangpenghui <zhangpenghui>
2017-11-15 18:49:26 +0800

    项目新增

---
+++ src/main/java/com/zph/usedCar/entity/ServiceEntityApplication.java
@@ -0,0 +1,12 @@
+package com.zph.usedCar.entity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceEntityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceEntityApplication.class, args);
+	}
+}
