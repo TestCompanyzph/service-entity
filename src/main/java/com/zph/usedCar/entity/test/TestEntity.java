96903c3956aa06ff1c2f8db8e8b9469762734143
zhangpenghui <zhangpenghui>
2017-11-16 14:23:54 +0800

    添加测试类

---
+++ src/main/java/com/zph/usedCar/entity/test/TestEntity.java
@@ -0,0 +1,18 @@
+package com.zph.usedCar.entity.test;
+
+import lombok.Data;
+
+/**
+ * @notes
+ * @autho zph
+ * @createTime 2017/11/16 12:06
+ */
+@Data
+public class TestEntity {
+
+    private Integer id;
+
+    private String code;
+
+    private String name;
+}
